0a0fca138fe472e643d699297566c66d947126d4
Anil Madhavapeddy <anil@recoil.org>
2012-09-28 19:25:58 -0700

    being porting mldig to Cmdliner instead of the ad-hoc Arg

--- lwt/mldig.ml
+++ lwt/mldig.ml
@@ -23,8 +23,6 @@ let debug x = if !debug_active then prerr_endline (sprintf "[debug] %s" x)
 let warn x y = prerr_endline (sprintf "%s warning: %s" x y)
 let error x y = prerr_endline (sprintf "%s error: %s" x y); exit(1)
 
-let got_alrm = ref false
-
 let print_timeout () =
   printf ";; connection timed out; no servers could be reached\n%!";
   exit 1
@@ -34,24 +32,20 @@ let print_section s = printf ";; %s SECTION:\n" (String.uppercase s)
 
 let print_answers p =
     printf ";; global options: \n";
-    let uc = String.uppercase in
-    let onl () = print_newline () in
-    let bitfn a b = if a then None else Some b in
     let { detail; id; questions; answers; authorities; additionals } = p in
+    let if_flag a b = if a then None else Some b in
     let flags = [
-      (match detail.qr with
-       |Query -> None
-       |Response -> Some "qr");
-      (bitfn detail.aa "aa");
-      (bitfn detail.tc "tc");
-      (bitfn detail.rd "rd");
-      (bitfn detail.ra "ra");
+      (match detail.qr with |Query -> None |Response -> Some "qr");
+      (if_flag detail.aa "aa");
+      (if_flag detail.tc "tc");
+      (if_flag detail.rd "rd");
+      (if_flag detail.ra "ra");
     ] in
-    let flags = String.concat " " 
-      (List.fold_left (fun a -> function |None -> a |Some x -> x :: a) [] flags) in
+    let flags = String.concat " " (List.fold_left (fun a ->
+      function |None -> a |Some x -> x :: a) [] flags) in
     printf ";; ->>HEADER<<- opcode: %s, status: %s, id: %u\n" 
-      (uc (opcode_to_string detail.opcode))
-      (uc (rcode_to_string detail.rcode)) id;
+      (String.uppercase (opcode_to_string detail.opcode))
+      (String.uppercase (rcode_to_string detail.rcode)) id;
     let al = List.length in
     printf ";; flags: %s; QUERY: %d, ANSWER: %d, AUTHORITY: %d, ADDITIONAL: %d\n\n"
       flags (al questions) (al answers) (al authorities) (al additionals);
@@ -62,11 +56,10 @@ let print_answers p =
         (q_class_to_string q.q_class)
         (q_type_to_string q.q_type)
       ) questions;
-      onl ();
+      print_newline ();
     end;
-    let print_rr a d e =
-      printf "%-24s %-8lu %-8s %-8s %s\n" 
-        (String.concat "." a.name) a.ttl (rr_class_to_string a.cls) d e in
+    let print_rr rr = printf "%-24s %-8lu %-8s %-8s %s\n" 
+        (String.concat "." rr.name) rr.ttl (rr_class_to_string rr.cls) in
     List.iter (fun (nm,ob) ->
       if al ob > 0 then print_section nm;
       List.iter (fun rr ->
@@ -82,67 +75,78 @@ let print_answers p =
         |NS a -> print_rr rr "NS" (String.concat "." a)
         |_ -> printf "unknown\n"
       ) ob;
-      if al ob > 0 then onl ()
+      if al ob > 0 then print_newline ()
     ) ["answer",answers; "authority",authorities; "additional",additionals]
   
 open Lwt
+open Cmdliner
+
+let dig res server source_ip dest_port q_class q_type args =
+  let timeout = 5 (* matches dig *) in
+  (* Fold over args to determine overrides for q_class/type *)
+  let (server, q_class, q_type, domains) = List.fold_left (
+    fun (server, q_class, q_type, domains) arg ->
+      (* Args beginning with @ decide the server *)
+      if String.length arg > 1 && (arg.[0] = '@') then begin
+        (* TODO: check for port? *)
+        let server = String.sub arg 1 (String.length arg -1) in
+        (Some server, q_class, q_type, domains)
+      end else begin
+        (* See if the argument is a query class or type *)
+        let arg = String.uppercase arg in
+        let q_type' = string_to_q_type arg in
+        let q_class' = string_to_q_class arg in
+        match q_type', q_class' with
+        |None, None -> (server, q_class, q_type, arg::domains)
+        |Some q_type, None -> (server, q_class, q_type, domains)
+        |None, Some q_class -> (server, q_class, q_type, domains)
+        |Some q_type, Some q_class -> (server, q_class, q_type, domains)
+      end
+  ) (server, q_class, q_type, []) args in
+  let domains = match domains with |[] -> ["."] |_ -> domains in
+  printf ";; <<>> MLDiG 1.0 <<>>\n"; (* TODO put query domains from Sys.argv here *)
+ match server with
+  |None -> error "dig" "Must specify a DNS resolver (with @<hostname>)"
+  |Some x -> 
+    debug (sprintf "Querying DNS server %s" x);
+    let domain = Dns.Name.string_to_domain_name (List.hd domains) in
+    let _ = Lwt_unix.sleep (float_of_int timeout) >|= print_timeout in
+    Dns_resolver.resolve res q_class q_type domain >|= print_answers
  
 let t =
   lwt res = Dns_resolver.create () in
   let (module Res) = res in
-  let (server,dest_port) =
+  let (default_server, default_dest_port) =
     match Res.servers with 
-    | [] -> None,53 
+    |[] -> None,53 
     |(s,p)::_ -> (Some s),p in
-  let server = ref server in
-  let dest_port = ref dest_port in
-  let args = ref [] in
-  let qty = ref Q_A in
-  let qcl = ref Q_IN in
-  let timeout = ref 2 in
-  let notimpl x = warn "dig" ("Command-line option \"" ^ x ^ "\" is not supported.") in
-  let spec = [
-    "-b", Arg.String (fun x -> notimpl "-b"), "set source IP of query";
-    "-c", Arg.String (fun x -> notimpl "-c"), "set query class (default IN)";
-    "-f", Arg.String (fun x -> notimpl "-f"), "read batched requests from file";
-    "-k", Arg.String (fun x -> notimpl "-k"), "sign outgoing queries";
-    "-p", Arg.Set_int dest_port, (sprintf "set destination port number (default %d)" !dest_port);
-    "-t", Arg.String (fun x -> notimpl "-t"), "set query type (default to A unless -x is set)";
-    "-x", Arg.String (fun x -> notimpl "-x"), "do reverse lookup";
-    "-y", Arg.String (fun x -> notimpl "-y"), "key to sign request";
-    "-v", Arg.Unit (fun () -> debug_active := true), "turn on debug output";
-  ] in
-    
-  let anon_arg x = 
-    if String.length x > 1 && (x.[0] = '@') then begin
-      server := Some (String.sub x 1 (String.length x - 1))
-    end else begin
-      match string_to_q_type (String.uppercase x) with
-      |None -> begin
-        match string_to_q_class (String.uppercase x) with
-        |None ->
-          args := !args @ [x];
-          debug (sprintf "Setting HOSTNAME = %s" x);
-        |Some cl ->
-          qcl := cl;
-          debug (sprintf "Setting CLASS = %s" (q_class_to_string !qcl));
-        end
-      |Some ty -> begin
-        qty := ty;
-        debug (sprintf "Setting TYPE = %s" (q_type_to_string !qty));
-      end
-    end
+  let source_ip =
+    Arg.(value & opt string "0.0.0.0" & info ["b"] ~docv:"SOURCE_IP"
+      ~doc:"set source IP of query") in
+  let dest_port =
+    Arg.(value & opt int default_dest_port & info ["p";"port"] ~docv:"DEST_PORT"
+      ~doc:"set destination port number") in
+  let q_class =
+    (fun x -> match string_to_q_class x with |None -> `Error "" |Some x -> `Ok x),
+    (fun f a -> Format.pp_print_string f (q_class_to_string a)) in
+  let q_class = Arg.(value & opt q_class Q_IN & info ["c";"qclass"]  ~docv:"QCLASS") in
+  let q_type =
+    (fun x -> match string_to_q_type x with |None -> `Error "" |Some x -> `Ok x),
+    (fun f a -> Format.pp_print_string f (q_type_to_string a)) in
+  let q_type = Arg.(value & opt q_type Q_A &  info ["t";"qtype"] ~docv:"QTYPE") in
+  let args = Arg.(non_empty & pos_all string [] & info [] ~docv:"ARGS") in
+  let info =
+    let doc = "DNS lookup utility" in
+    let man = [ `S "BUGS"; `P "Email bug reports to <cl-mirage@lists.cl.cam.ac.uk>."] in
+    Term.info "mldig" ~version:"1.0.0" ~doc ~man
   in
-  Arg.parse spec anon_arg "generate and send DNS queries";
-  if !args = [] then error "dig" "Must specify at least one hostname to resolve.";
-  if List.length !args > 1 then error "dig" "Only one name can be resolved at a time.";
-  printf ";; <<>> MLDiG 1.0 <<>>\n"; (* TODO put query domains from Sys.argv here *)
-  match !server with
-  |None -> error "dig" "Must specify a DNS resolver (with @<hostname>)"
-  |Some x -> 
-    debug (sprintf "Querying DNS server %s" x);
-    let domain = Dns.Name.string_to_domain_name (List.hd !args) in
-    let _ = Lwt_unix.sleep (float_of_int !timeout) >|= print_timeout in
-    Dns_resolver.resolve res !qcl !qty domain >|= print_answers
+  let cmd_t = Term.(pure 
+    (dig res default_server) $ 
+    source_ip $ 
+    dest_port $ 
+    q_class $ 
+    q_type $ args) in
+  match Term.eval (cmd_t, info) with `Ok x -> x |_ -> exit 1
 
 let _ = Lwt_main.run t
+
