b123dc9f372fea381dec8c6456701a6d59e02a83
Anil Madhavapeddy <anil@recoil.org>
2013-01-13 11:53:57 +0000

    bump to 0.6.1 working version and add Mirage subpackage

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: cbd110f6015746a9625cba2e3e69d6cc) *)
+(* DO NOT EDIT (digest: 46b7a0667fc5d672e85dadd699454ccc) *)
 module OASISGettext = struct
-(* # 21 "/Users/avsm/.opam/system/build/oasis-mirage.0.3.0a/src/oasis/OASISGettext.ml" *)
+(* # 21 "/home/avsm/.opam/4.01.0dev+mirage-xen/build/oasis-mirage.0.3.0a/src/oasis/OASISGettext.ml" *)
 
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-(* # 21 "/Users/avsm/.opam/system/build/oasis-mirage.0.3.0a/src/oasis/OASISExpr.ml" *)
+(* # 21 "/home/avsm/.opam/4.01.0dev+mirage-xen/build/oasis-mirage.0.3.0a/src/oasis/OASISExpr.ml" *)
 
 
 
@@ -116,7 +116,7 @@ end
 
 # 117 "myocamlbuild.ml"
 module BaseEnvLight = struct
-(* # 21 "/Users/avsm/.opam/system/build/oasis-mirage.0.3.0a/src/base/BaseEnvLight.ml" *)
+(* # 21 "/home/avsm/.opam/4.01.0dev+mirage-xen/build/oasis-mirage.0.3.0a/src/base/BaseEnvLight.ml" *)
 
   module MapString = Map.Make(String)
 
@@ -214,7 +214,7 @@ end
 
 # 215 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
-(* # 21 "/Users/avsm/.opam/system/build/oasis-mirage.0.3.0a/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
+(* # 21 "/home/avsm/.opam/4.01.0dev+mirage-xen/build/oasis-mirage.0.3.0a/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -335,7 +335,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-(* # 21 "/Users/avsm/.opam/system/build/oasis-mirage.0.3.0a/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 21 "/home/avsm/.opam/4.01.0dev+mirage-xen/build/oasis-mirage.0.3.0a/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -351,7 +351,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
 
-(* # 56 "/Users/avsm/.opam/system/build/oasis-mirage.0.3.0a/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
+(* # 56 "/home/avsm/.opam/4.01.0dev+mirage-xen/build/oasis-mirage.0.3.0a/src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
   type t =
       {
@@ -507,7 +507,8 @@ end
 open Ocamlbuild_plugin;;
 let package_default =
   {
-     MyOCamlbuildBase.lib_ocaml = [("dns", ["lib"]); ("lwt", ["lwt"])];
+     MyOCamlbuildBase.lib_ocaml =
+       [("dns", ["lib"]); ("lwt", ["lwt"]); ("mirage", ["mirage"])];
      lib_c = [("lwt", "lwt", [])];
      flags =
        [
@@ -516,12 +517,13 @@ let package_default =
           (["oasis_library_lwt_cclib"; "ocamlmklib"; "c"],
             [(OASISExpr.EBool true, S [A "-lcrypto"])])
        ];
-     includes = [("lwt", ["lib"]); ("lib_test", ["lib"; "lwt"])];
+     includes =
+       [("mirage", ["lib"]); ("lwt", ["lib"]); ("lib_test", ["lib"; "lwt"])];
      }
   ;;
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 526 "myocamlbuild.ml"
+# 528 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
