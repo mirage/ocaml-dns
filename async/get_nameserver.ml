3ca7c74c0086dea6accf99c5c2638e4d0596a25e
heidi-ann <hh360@cam.ac.uk>
2013-07-03 13:40:06 +0100

    adding first bit of async code in get_resolver.ml, currently not working

---
+++ async/get_nameserver.ml
@@ -0,0 +1,39 @@
+open Core.Std
+open Async.Std
+open Async_unix
+open Printf
+open Dns.Resolvconf
+
+
+  let default_configuration_file = "/etc/resolv.conf"
+
+  let get_resolvers ?(file=default_configuration_file) () =
+    Unix_syscalls.with_file ~mode:[`Rdonly]  file ~f:(fun fd ->
+      let warn x = prerr_endline (Printf.sprintf "resolvconf in file %s: %s" file x) in
+      let reader = Reader.create fd in
+      let rec input_lines (res : string list ) : string list Deferred.t = 
+        Reader.read_line reader
+	>>= function 
+	  | `Ok x -> input_lines (x::res)
+	  | `Eof ->  return res  in
+      input_lines []
+        >>| List.filter_map ~f:map_line
+        >>| List.filter_map ~f:(fun line ->
+        try Some (KeywordValue.of_string line)
+        with
+        | KeywordValue.Unknown x -> warn ("unknown keyword: " ^ x); None
+        | OptionsValue.Unknown x -> warn ("unknown option: " ^ x); None
+        | LookupValue.Unknown x  -> warn ("unknown lookup option: " ^ x); None
+      ))
+    
+
+ let run ()  =  
+  upon (get_resolvers())  (fun (res:KeywordValue.t list) -> 
+	let servers = all_servers res in
+         match servers with  | (server,_)::_ -> Printf.printf "Your nameserver is %S" server
+			    | _ -> Printf.printf "o no :(" ) 
+
+ let _ =
+  let _ = run (); in
+   Printf.printf "starting...\n%!";
+   Scheduler.go ()
