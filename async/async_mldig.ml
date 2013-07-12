275a7073def7e49cac8e2ba7d0549e7b0e72a489
heidi-ann <hh360@cam.ac.uk>
2013-07-12 19:48:47 +0100

    moving code and simpliying

--- async/async_mldig.ml
+++ async/async_mldig.ml
@@ -24,9 +24,9 @@ open Dns.Packet
 open Uri
 
 let debug_active = ref false
-let debug x = if !debug_active then prerr_endline (sprintf "[debug] %s" x)
-let warn x y = prerr_endline (sprintf "%s warning: %s" x y)
-let error x y = prerr_endline (sprintf "%s error: %s" x y); exit(1)
+let debug x = if !debug_active then (printf "[debug] %s \n" x)
+let warn x y = (printf "%s warning: %s\n" x y)
+let error x y = (printf "%s error: %s \n" x y)
 
 let print_timeout () =
   printf ";; connection timed out; no servers could be reached\n%!";
@@ -84,76 +84,24 @@ let print_answers p =
   
 open Cmdliner
 
-let dig server source_ip dest_port q_class q_type args =
-  let timeout = 5 (* matches dig *) in
-  (* Fold over args to determine overrides for q_class/type *)
-  let (server, q_class, q_type, domains) = List.fold_left ~f:(
-    fun (server, q_class, q_type, domains) arg ->
-      (* Args beginning with @ decide the server *)
-      if String.length arg > 1 && (arg.[0] = '@') then begin
-        (* TODO: check for port? *)
-        let server = String.sub arg 1 (String.length arg -1) in
-        (Some server, q_class, q_type, domains)
-      end else begin
-        (* See if the argument is a query class or type *)
-        let arg = String.uppercase arg in
-        let q_type' = string_to_q_type arg in
-        let q_class' = string_to_q_class arg in
-        match q_type', q_class' with
-        |None, None -> (server, q_class, q_type, arg::domains)
-        |Some q_type, None -> (server, q_class, q_type, domains)
-        |None, Some q_class -> (server, q_class, q_type, domains)
-        |Some q_type, Some q_class -> (server, q_class, q_type, domains)
-      end
-  ) ~init:(server, q_class, q_type, []) args in
-  let domains = match domains with |[] -> ["."] |_ -> domains in
-  printf ";; <<>> MLDiG 1.0 <<>>\n"; (* TODO put query domains from Sys.argv here *)
- match server with
-  |None -> error "dig" "Must specify a DNS resolver (with @<hostname>)"
-  |Some x -> 
-    debug (sprintf "Querying DNS server %s" x);
-    let domain = (match (List.hd domains) with Some x -> x | None -> "www.google.co.uk") in
+let run domain :unit =
+    let q_type = string_to_q_type "A" in
+    let q_class = string_to_q_class "IN" in
     let domain = string_to_domain_name domain in
     Async_dns_resolver.get_resolvers ()
-    >>= (match (fun resolvers -> Async_dns_resolver.resolve resolvers q_class q_type domain)
-      with | None ->  printf ":(" 
-           | Some pkt -> (pkt >>> print_answers))
- 
-let t =
-  let (default_server, default_dest_port) =
-    Dns_resolver.get_config ()
-    >>| (function |[] -> None,53 
-   		  |(s,p)::_ -> (Some s),p ) in
-  let source_ip =
-    Arg.(value & opt string "0.0.0.0" & info ["b"] ~docv:"SOURCE_IP"
-      ~doc:"set source IP of query") in
-  let dest_port =
-    Arg.(value & opt (some int) None & info ["p";"port"] ~docv:"DEST_PORT"
-      ~doc:"set destination port number") in
-  let q_class =
-    (fun x -> match string_to_q_class x with |None -> `Error "" |Some x -> `Ok x),
-    (fun f a -> Format.pp_print_string f (q_class_to_string a)) in
-  let q_class = Arg.(value & opt q_class Q_IN & info ["c";"qclass"]  ~docv:"QCLASS") in
-  let q_type =
-    (fun x -> match string_to_q_type x with |None -> `Error "" |Some x -> `Ok x),
-    (fun f a -> Format.pp_print_string f (q_type_to_string a)) in
-  let q_type = Arg.(value & opt q_type Q_A &  info ["t";"qtype"] ~docv:"QTYPE") in
-  let args = Arg.(non_empty & pos_all string [] & info [] ~docv:"ARGS") in
-  let info =
-    let doc = "DNS lookup utility" in
-    let man = [ `S "BUGS"; `P "Email bug reports to <cl-mirage@lists.cl.cam.ac.uk>."] in
-    Term.info "mldig" ~version:"1.0.0" ~doc ~man
-  in
-  let cmd_t = Term.(pure 
-    (dig default_server) $ 
-    source_ip $ 
-    dest_port $ 
-    q_class $ 
-    q_type $ args) in
-  match Term.eval (cmd_t, info) with `Ok x -> x |_ -> Caml.exit 1
+    >>= (fun resolvers -> Async_dns_resolver.resolve resolvers q_class q_type domain)
+    >>> (function | `Result p -> (print_answers p ; Caml.exit 0) 
+		  | `Timeout -> printf ":("; Caml.exit 1  )
 
- let _ =
-  let _ = run (); in
-   Printf.printf "starting...\n%!";
-   Scheduler.go ()
+
+let () =
+  Command.async_basic
+    ~summary:"Start an dns resquest"
+    Command.Spec.(
+      empty
+      +> flag "-domain" (optional_with_default "www.bbc.co.uk" string)
+        ~doc:" Domain to query (default www.bbc.co.uk)"
+    )
+    (fun uppercase port () -> run ~domain)
+  |> Command.run
 
