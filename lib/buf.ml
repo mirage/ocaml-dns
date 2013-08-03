99e179d5a65bb6cc9adc81efb0688e82cf069ab4
David Sheets <sheets@alum.mit.edu>
2013-08-03 16:38:13 +0100

    A minimal nickname for Bigarray character buffers

---
+++ lib/buf.ml
@@ -0,0 +1,9 @@
+module B = Bigarray
+module B1 = B.Array1
+
+type t = (char, B.int8_unsigned_elt, B.c_layout) B1.t
+
+let create = B1.create B.char B.c_layout
+let length = B1.dim
+let of_cstruct c = Cstruct.(B1.sub c.buffer c.off c.len)
+let shift b k = B1.sub b k (length b - k)
---
+++ lib/buf.mli
@@ -0,0 +1,5 @@
+type t = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+val create : int -> t
+val length : t -> int
+val of_cstruct : Cstruct.t -> t
+val shift : t -> int -> t
