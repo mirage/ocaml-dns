cbfa1ab6b0c601ecd593295b4710307ae2c46c88
Anil Madhavapeddy <anil@recoil.org>
2013-09-28 18:14:48 +0100

    travis: add cmdliner dep

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt cstruct ipaddr re ounit mirage-net async"
+OPAM_DEPENDS="lwt cstruct ipaddr re ounit mirage-net async cmdliner"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
@@ -15,7 +15,6 @@ sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 
 export OPAMYES=1
-export OPAMVERBOSE=1
 echo OCaml version
 ocaml -version
 echo OPAM versions
