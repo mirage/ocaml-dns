cef5bf66387057f975c003b80f2ff04de52180cd
Anil Madhavapeddy <anil@recoil.org>
2014-06-17 00:11:32 +0100

    Ensure that all `Dns.Buf.t` buffers are page-aligned, via `Io_page`.
    This fixes the DNS library under Mirage/Xen, which zero copies the
    Dns.Buf.t and requires this invariant.

--- lib/buf.ml
+++ lib/buf.ml
@@ -20,7 +20,7 @@ module B1 = B.Array1
 
 type t = (char, B.int8_unsigned_elt, B.c_layout) B1.t
 
-let create = B1.create B.char B.c_layout
+let create len = B1.sub (Io_page.get 1) 0 len
 let length = B1.dim
 let of_cstruct c = Cstruct.(B1.sub c.buffer c.off c.len)
 let shift b k = B1.sub b k (length b - k)
