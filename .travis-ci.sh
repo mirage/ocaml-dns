0c36e461e7bb896aed005f7f581334cd834a4e04
Anil Madhavapeddy <anil@recoil.org>
2014-06-17 00:27:44 +0100

    travis: +io-page

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="lwt cstruct ipaddr re ounit cmdliner"
+OPAM_DEPENDS="lwt cstruct ipaddr re ounit cmdliner io-page"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
