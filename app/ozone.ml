638838a169cefa20969532468f81a824f2725022
Hannes Mehnert <hannes@mehnert.org>
2019-04-07 00:23:57 +0200

    zonefile -> zone

--- app/ozone.ml
+++ app/ozone.ml
@@ -12,7 +12,7 @@ let err_to_msg = function Ok () -> Ok () | Error e -> Error (`Msg (Fmt.to_to_str
 
 let load_zone zone =
   Bos.OS.File.read Fpath.(v zone) >>= fun data ->
-  Udns_zonefile.load data >>| fun zone ->
+  Udns_zone.parse data >>| fun zone ->
   Udns_trie.insert_map zone Udns_trie.empty
 
 let jump _ zone old =
