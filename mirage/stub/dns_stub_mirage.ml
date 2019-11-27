f3b6a803f16e8840d477263e08c598fac8d3ab7b
Hannes Mehnert <hannes@mehnert.org>
2019-11-27 14:10:30 +0100

    stub: embed client code, use single connection to resolver

--- mirage/stub/dns_stub_mirage.ml
+++ mirage/stub/dns_stub_mirage.ml
@@ -3,8 +3,110 @@ open Lwt.Infix
 
 open Dns
 
+let src = Logs.Src.create "dns_stub_mirage" ~doc:"effectful DNS stub layer"
+module Log = (val Logs.src_log src : Logs.LOG)
+
 module Make (R : Mirage_random.S) (C : Mirage_clock.MCLOCK) (S : Mirage_stack.V4) = struct
-  module Client = Dns_client_mirage.Make(R)(C)(S)
+
+  module Client = struct
+    module Transport : Dns_client.S
+      with type stack = S.t
+       and type +'a io = 'a Lwt.t
+       and type io_addr = Ipaddr.V4.t * int = struct
+      type stack = S.t
+      type io_addr = Ipaddr.V4.t * int
+      type ns_addr = [`TCP | `UDP] * io_addr
+      type +'a io = 'a Lwt.t
+      type t = {
+        rng : (int -> Cstruct.t) ;
+        nameserver : ns_addr ;
+        stack : stack ;
+        mutable flow : S.TCPV4.flow option ;
+      }
+      type flow = t
+
+      let create
+          ?rng
+          ?(nameserver = `TCP, (Ipaddr.V4.of_string_exn "91.239.100.100", 53))
+          stack =
+        let rng = match rng with None -> R.generate ?g:None | Some x -> x in
+        { rng ; nameserver ; stack ; flow = None }
+
+      let nameserver { nameserver ; _ } = nameserver
+      let rng { rng ; _ } = rng
+
+      let bind = Lwt.bind
+      let lift = Lwt.return
+
+      let connect ?nameserver:ns t =
+        match t.flow with
+        | Some _ -> Lwt.return (Ok t)
+        | None ->
+          let _proto, addr = match ns with None -> nameserver t | Some x -> x in
+          S.TCPV4.create_connection (S.tcpv4 t.stack) addr >|= function
+          | Error e ->
+            Log.err (fun m -> m "error connecting to nameserver %a"
+                        S.TCPV4.pp_error e) ;
+            Error (`Msg "connect failure")
+          | Ok flow ->
+            t.flow <- Some flow;
+            Ok t
+
+      let close _f =
+        (* ignoring this here *)
+        Lwt.return_unit
+
+      let recv t =
+        match t.flow with
+        | None -> Lwt.return (Error (`Msg "no connected flow"))
+        | Some flow ->
+          S.TCPV4.read flow >|= function
+          | Error e -> t.flow <- None; Error (`Msg (Fmt.to_to_string S.TCPV4.pp_error e))
+          | Ok (`Data cs) -> Ok cs
+          | Ok `Eof -> Ok Cstruct.empty
+
+      let send t s =
+        (match t.flow with
+         | Some flow -> Lwt.return (Ok flow)
+         | None -> connect t >|= function
+           | Ok _ ->
+             begin match t.flow with
+               | Some flow -> Ok flow
+               | None -> Error (`Msg "couldn't establish connection to resolver")
+             end
+           | Error e -> Error e) >>= function
+        | Error e -> Lwt.return (Error e)
+        | Ok flow ->
+          S.TCPV4.write flow s >|= function
+          | Error e ->
+            t.flow <- None;
+            Error (`Msg (Fmt.to_to_string S.TCPV4.pp_write_error e))
+          | Ok () -> Ok ()
+    end
+
+    include Dns_client.Make(Transport)
+
+    let create ?size ?nameserver stack =
+      create ?size ~rng:R.generate ?nameserver ~clock:C.elapsed_ns stack
+  end
+
+  (* likely this should contain:
+     - a primary server (handling updates)
+     - a client on steroids: multiplexing on connections
+     - listening for DNS requests from clients:
+        first find them in primary server
+        if not authoritative, use the client
+  *)
+
+  (* task management
+     - multiple requests for the same name, type can be done at the same "time"
+     -> need to remember outstanding requests and signal to clients
+  *)
+
+  (* multiplex TCP connection to resolver *)
+
+  (* take multiple resolver IPs and round-robin / ask both (take first answer,
+     ignoring ServFail etc.) *)
 
   type t = {
     client : Client.t ;
@@ -17,6 +119,7 @@ module Make (R : Mirage_random.S) (C : Mirage_clock.MCLOCK) (S : Mirage_stack.V4
       Logs.err (fun m -> m "couldn't decode %a" Packet.pp_err err);
       Lwt.return None
     | Ok packet ->
+      (* check header flags: recursion desired (and send recursion available) *)
       let name = fst packet.Packet.question in
       match packet.Packet.data, snd packet.Packet.question with
       | `Query, `K K key ->
