a7426d62d8702d04902bf937bb0527f0c5afe51e
Hannes Mehnert <hannes@mehnert.org>
2021-11-24 22:12:10 +0100

    dnssec: avoid x509 dependency

--- dnssec/dnssec.ml
+++ dnssec/dnssec.ml
@@ -4,6 +4,19 @@ let ( let* ) = Result.bind
 
 let guard a e = if a then Ok () else Error e
 
+type pub = [
+  | `P256 of Mirage_crypto_ec.P256.Dsa.pub
+  | `P384 of Mirage_crypto_ec.P384.Dsa.pub
+  | `ED25519 of Mirage_crypto_ec.Ed25519.pub
+  | `RSA of Mirage_crypto_pk.Rsa.pub
+]
+
+let pp_pub ppf = function
+  | `P256 _ -> Fmt.string ppf "P256"
+  | `P384 _ -> Fmt.string ppf "P384"
+  | `ED25519 _ -> Fmt.string ppf "ED25519"
+  | `RSA k -> Fmt.pf ppf "RSA %d bits" (Mirage_crypto_pk.Rsa.pub_bits k)
+
 (* used by DS, RFC 4034 section 5.1.4 *)
 let digest algorithm owner dnskey =
   let* h =
@@ -47,11 +60,11 @@ let dnskey_to_pk { Dnskey.algorithm ; key ; _ } =
   | MD5 | SHA1 | SHA224 | SHA256 | SHA384 | SHA512 | Unknown _ ->
     Error (`Msg (Fmt.str "unsupported key algorithm %a" Dnskey.pp_algorithm algorithm))
 
-let verify : type a . Ptime.t -> X509.Public_key.t -> [`raw] Domain_name.t -> Rrsig.t ->
+let verify : type a . Ptime.t -> pub -> [`raw] Domain_name.t -> Rrsig.t ->
   a Rr_map.key -> a -> (unit, [> `Msg of string ]) result = fun now key name rrsig t v ->
   (* from RFC 4034 section 3.1.8.1 *)
-  Logs.info (fun m -> m "verifying for %a (with %a / %a)" Domain_name.pp name 
-    X509.Key_type.pp (X509.Public_key.key_type key)
+  Logs.info (fun m -> m "verifying for %a (with %a / %a)" Domain_name.pp name
+    pp_pub key
     Dnskey.pp_algorithm rrsig.Rrsig.algorithm);
   let* algorithm =
     match rrsig.Rrsig.algorithm with
@@ -90,4 +103,3 @@ let verify : type a . Ptime.t -> X509.Public_key.t -> [`raw] Domain_name.t -> Rr
     Logs.info (fun m -> m "digest %a" Cstruct.hexdump_pp (Mirage_crypto.Hash.digest algorithm data));
 
     ok_if_true (Mirage_crypto_pk.Rsa.PKCS1.verify ~hashp ~key ~signature:rrsig.Rrsig.signature (`Message data))
-  | _ -> Error (`Msg "unsupported key")
