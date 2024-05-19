96382439335bb3737cf7149bd6f257dbb8531564
Hannes Mehnert <hannes@mehnert.org>
2024-05-19 17:38:52 +0200

    minor

--- lwt/client/dns_client_lwt.mli
+++ lwt/client/dns_client_lwt.mli
@@ -22,6 +22,6 @@ val create_happy_eyeballs :
   ?timer_interval:int64 ->
   t ->
   Happy_eyeballs_lwt.t
-(** [create_happy_eyeballs t] returns the happy-eyeballs instance used by the
+(** [create_happy_eyeballs dns] returns the happy-eyeballs instance used by the
     [dns] implementation, which uses the [dns] implementation for resolving
     hostnames. *)
