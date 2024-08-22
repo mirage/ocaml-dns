fe5889b950a1ff1589b14fbb1d3d883c35421fb0
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-08-22 18:28:38 +0200

    Implement the DNS protocol on the Miou implementation (it was previously into the happy-eyeballs implementation)

--- miou/client/dns_client_miou_unix.mli
+++ miou/client/dns_client_miou_unix.mli
@@ -1,6 +1,6 @@
 module Transport : Dns_client.S
   with type io_addr = [ `Plaintext of Ipaddr.t * int | `Tls of Tls.Config.client * Ipaddr.t * int ]
    and type +'a io = 'a
-   and type stack = Happy_eyeballs_miou_unix.happy
+   and type stack = Happy_eyeballs_miou_unix.t
 
 include module type of Dns_client.Make (Transport)
