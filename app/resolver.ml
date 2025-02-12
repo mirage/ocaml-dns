bf546bbd4dfc94d3578b55a32046471b17b37700
Reynir Björnsson <reynir@reynir.dk>
2025-02-12 15:03:56 +0100

    Update dns-cli/resolver for mirage-crypto etc

--- app/resolver.ml
+++ app/resolver.ml
@@ -1,11 +1,10 @@
 
-module Rng = Mirage_crypto_rng_mirage.Make(Unix_os.Time)(Mclock)
-module Resolver = Dns_resolver_mirage.Make(Rng)(Pclock)(Mclock)(Unix_os.Time)(Tcpip_stack_socket.V4V6)
+module Resolver = Dns_resolver_mirage.Make(Tcpip_stack_socket.V4V6)
 
 open Lwt.Infix
 
 let main () =
-  Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna);
+  Mirage_crypto_rng_unix.use_default ();
   Logs.set_level (Some Debug) ;
   Logs.set_reporter (Logs_fmt.reporter ());
   Udpv4v6_socket.connect ~ipv4_only:true ~ipv6_only:false Ipaddr.V4.Prefix.global None >>= fun udp ->
@@ -17,7 +16,7 @@ let main () =
       Dns_server.Primary.create ~rng:Mirage_crypto_rng.generate Dns_trie.empty
     in
     Dns_resolver.create ~dnssec:true ~ip_protocol:`Ipv4_only
-      (Mclock.elapsed_ns ()) Mirage_crypto_rng.generate primary_t
+      (Mirage_mtime.elapsed_ns ()) Mirage_crypto_rng.generate primary_t
   in 
   Resolver.resolver ~port:5353 stack resolver;
   Tcpip_stack_socket.V4V6.listen stack
