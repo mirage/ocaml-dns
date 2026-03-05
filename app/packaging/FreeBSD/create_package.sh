2cadd9906f031fc664087d862e61186424de3c3d
Hannes Mehnert <hannes@mehnert.org>
2026-03-05 12:07:57 +0100

    Add packaging (FreeBSD, debian) for dns-cli (oupdate, onotify, ozone) (#414)
    * Add packaging (FreeBSD, debian) for dns-cli (oupdate, onotify, ozone)
    
    * dns-cli.opam: add more description for the binaries that are part of dns-cli

---
+++ app/packaging/FreeBSD/create_package.sh
@@ -0,0 +1,44 @@
+#!/bin/sh -e
+
+# only execute anything if either
+# - running under orb with package = dns-cli
+# - not running under opam at all
+if [ "$ORB_BUILDING_PACKAGE" != "dns-cli" -a "$OPAM_PACKAGE_NAME" != "" ]; then
+    exit 0;
+fi
+
+basedir=$(realpath "$(dirname "$0")"/../../..)
+pdir=$basedir/app/packaging/FreeBSD
+bdir=$basedir/_build/install/default/bin
+tmpd=$basedir/_build/stage
+manifest=$tmpd/+MANIFEST
+rootdir=$tmpd/rootdir
+bindir=$rootdir/usr/local/bin
+
+trap 'rm -rf $tmpd' 0 INT EXIT
+
+mkdir -p "$bindir"
+
+# stage app binaries
+for f in onotify oupdate ozone
+do install -U $bdir/$f $bindir/$f; done
+
+# create +MANIFEST
+flatsize=$(find "$rootdir" -type f -exec stat -f %z {} + |
+               awk 'BEGIN {s=0} {s+=$1} END {print s}')
+
+sed -e "s:%%FLATSIZE%%:${flatsize}:" -e "/^[Vv]ersion:/s/-/./g" "$pdir/MANIFEST" > "$manifest"
+
+{
+    printf '\nfiles {\n'
+    find "$rootdir" -type f -exec sha256 -r {} + | sort |
+        awk '{print "    " $2 ": \"" $1 "\"," }'
+    find "$rootdir" -type l | sort |
+        awk "{print \"    \"\$1 \": -,\"}"
+    printf '}\n'
+} | sed -e "s:${rootdir}::" >> "$manifest"
+
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=format:%ct)
+pkg create -r "$rootdir" -M "$manifest" -o $basedir/
+mv $basedir/dns-cli-* $basedir/dns-cli.pkg
+echo 'bin: [ "dns-cli.pkg" ]' > $basedir/dns-cli.install
